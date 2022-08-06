eff5d699fdca9f24ce0466c6c09eba7006781750
Ariane de Almeida Franchetto <nanifranchetto@gmail.com>
2022-08-06 11:55:26 -0300

    Initial commit

---
+++ rpg/src/App.tsx
@@ -0,0 +1,49 @@
+import { useEffect } from 'react';
+import * as C from './App.styles';
+import { Character } from './components/Character';
+import { useCharacter } from './hooks/qlqr2/useCharacter';
+
+
+const App = () => {
+  const char = useCharacter();
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+}, []);
+
+const handleKeyDown = (e: KeyboardEvent) => {
+  switch(e.code) {
+    case 'KeyA':
+    case 'ArrowLeft':
+      char.moveLeft();
+    break;
+    case 'KeyW':
+    case 'ArrowUp':
+      char.moveUp();
+    break;
+    case 'KeyD':
+    case 'ArrowRight':
+      char.moveRight();
+    break;
+    case 'KeyS':
+    case 'ArrowDown':
+      char.moveDown();
+    break;
+  
+    
+  }
+  }
+  
+  
+
+  return (
+    <C.Container>
+    <C.Map>
+      <Character x={char.x} y={char.y}/>
+      </C.Map>
+    </C.Container>
+
+  );
+}
+
+export default App;
