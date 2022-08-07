104f80aabf1f7617d41e75003f42c4b950d90fa3
Ariane de Almeida Franchetto <nanifranchetto@gmail.com>
2022-08-07 14:41:58 -0300

    melhorias
    adicionado um limite para que o boneco conseguisse andar apenas nos lugares permitidos
    inicialização de um chat

--- rpg/src/App.tsx
+++ rpg/src/App.tsx
@@ -5,44 +5,41 @@ import { useCharacter } from './hooks/qlqr2/useCharacter';
 
 
 const App = () => {
-  const char = useCharacter();
+  const char = useCharacter('Nani');
+ 
 
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
-}, []);
+  }, []);
 
-const handleKeyDown = (e: KeyboardEvent) => {
-  switch(e.code) {
-    case 'KeyA':
-    case 'ArrowLeft':
-      char.moveLeft();
-    break;
-    case 'KeyW':
-    case 'ArrowUp':
-      char.moveUp();
-    break;
-    case 'KeyD':
-    case 'ArrowRight':
-      char.moveRight();
-    break;
-    case 'KeyS':
-    case 'ArrowDown':
-      char.moveDown();
-    break;
-  
-    
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch(e.code) {
+      case 'KeyA':
+      case 'ArrowLeft':
+        char.moveLeft();
+      break;
+      case 'KeyW':
+      case 'ArrowUp':
+        char.moveUp();
+      break;
+      case 'KeyD':
+      case 'ArrowRight':
+        char.moveRight();
+      break;
+      case 'KeyS':
+      case 'ArrowDown':
+        char.moveDown();
+      break;
+    }
   }
-  }
-  
-  
 
   return (
     <C.Container>
-    <C.Map>
-      <Character x={char.x} y={char.y}/>
+      <C.Map>
+        <Character x={char.x} y={char.y} side={char.side} name={char.name} />
+       
       </C.Map>
     </C.Container>
-
   );
 }
 
